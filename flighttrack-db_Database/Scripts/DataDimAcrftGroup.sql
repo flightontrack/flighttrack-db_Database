b2ae91cf2397cd3f85d6fa9f08b368673168d326
flightontrack <flightontrack@gmail.com>
2017-04-25 22:46:34 -0400

    q

--- flighttrack-db_Database/Scripts/DataDimAcrftGroup.sql
+++ flighttrack-db_Database/Scripts/DataDimAcrftGroup.sql
@@ -2,3 +2,10 @@
 INSERT INTO [dbo].[DimAcftGroup] ([GroupID], [GroupName],GroupType) VALUES (1, N'Urban Aviation','company')
 INSERT INTO [dbo].[DimAcftGroup] ([GroupID], [GroupName],GroupType) VALUES (2, N'Thunderbirds','formation')
 
+insert into AircraftGroup(AcftID,GroupID) values (2,1)
+insert into AircraftGroup(AcftID,GroupID) values (8,1)
+insert into AircraftGroup(AcftID,GroupID) values (9,1)
+insert into AircraftGroup(AcftID,GroupID) values (7,2)
+insert into AircraftGroup(AcftID,GroupID) values (8,2)
+
+
