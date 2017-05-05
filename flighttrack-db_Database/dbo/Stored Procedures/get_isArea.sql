b1bb1e0d4fe060e683cc002d7b53791406e23e92
Tikhomirov <Tikhomirov@TIKHOMIR41360>
2017-05-05 08:45:45 -0400

    w

--- flighttrack-db_Database/dbo/Stored Procedures/get_isArea.sql
+++ flighttrack-db_Database/dbo/Stored Procedures/get_isArea.sql
@@ -1,15 +1,43 @@
 ﻿CREATE PROC [dbo].[get_isArea] 
 (
 	@GpsLocationID int,
-	@AreaID int
+	@AreaID int,
+	@Radius char(2)=null
 )
 AS
 	--DECLARE @Result int
+	IF @Radius = '3'
+		SELECT sArea= ISNULL(t2.GeoLocbuffer.STIntersects(t1.GeoLocation),0)
+		FROM GpsLocation t1
+		LEFT JOIN DimArea t2 
+		ON t2.GeoLocbuffer_3.STIntersects(t1.GeoLocation)=1
+		WHERE t1.GpsLocationID=@GpsLocationID AND t2.AreaID=@AreaID
+	
+	IF @Radius = '5'
+		SELECT sArea= ISNULL(t2.GeoLocbuffer.STIntersects(t1.GeoLocation),0)
+		FROM GpsLocation t1
+		LEFT JOIN DimArea t2 
+		ON t2.GeoLocbuffer_5.STIntersects(t1.GeoLocation)=1
+		WHERE t1.GpsLocationID=@GpsLocationID AND t2.AreaID=@AreaID
 
-	SELECT sArea= ISNULL(t2.GeoLocbuffer.STIntersects(t1.GeoLocation),0)
-	FROM GpsLocation t1
-	LEFT JOIN DimArea t2 
-	ON t2.GeoLocbuffer.STIntersects(t1.GeoLocation)=1
-	WHERE t1.GpsLocationID=@GpsLocationID AND t2.AreaID=@AreaID
+	IF @Radius = '10'
+		SELECT sArea= ISNULL(t2.GeoLocbuffer.STIntersects(t1.GeoLocation),0)
+		FROM GpsLocation t1
+		LEFT JOIN DimArea t2 
+		ON t2.GeoLocbuffer_10.STIntersects(t1.GeoLocation)=1
+		WHERE t1.GpsLocationID=@GpsLocationID AND t2.AreaID=@AreaID
 
+	IF @Radius = '15'
+		SELECT sArea= ISNULL(t2.GeoLocbuffer.STIntersects(t1.GeoLocation),0)
+		FROM GpsLocation t1
+		LEFT JOIN DimArea t2 
+		ON t2.GeoLocbuffer_15.STIntersects(t1.GeoLocation)=1
+		WHERE t1.GpsLocationID=@GpsLocationID AND t2.AreaID=@AreaID
+
+	ELSE
+		SELECT sArea= ISNULL(t2.GeoLocbuffer.STIntersects(t1.GeoLocation),0)
+		FROM GpsLocation t1
+		LEFT JOIN DimArea t2 
+		ON t2.GeoLocbuffer.STIntersects(t1.GeoLocation)=1
+		WHERE t1.GpsLocationID=@GpsLocationID AND t2.AreaID=@AreaID
 	--RETURN @Result
--- flighttrack-db_Database/dbo/Tables/DimArea.sql
+++ flighttrack-db_Database/dbo/Tables/DimArea.sql
@@ -1,11 +1,21 @@
 ﻿CREATE TABLE [dbo].[DimArea] (
-    [AreaID]       INT             IDENTITY (1, 1) NOT NULL,
-    [AreaName]     VARCHAR (250)   NULL,
-    [CenterLat]    DECIMAL (13, 8) NULL,
-    [CenterLong]   DECIMAL (13, 8) NULL,
-    [CenterPoint]  AS              ([geography]::Point([CenterLat],[CenterLong],(4326))) PERSISTED,
-    [Radius]       INT             NULL,
-    [Area]         AS              ([geography]::Point([CenterLat],[CenterLong],(4326)).STBuffer([Radius])) PERSISTED,
-    [GeoLocBuffer] AS              ([geography]::Point([CenterLat],[CenterLong],(4326)).STBuffer([Radius])) PERSISTED
+    [AreaID]          INT             IDENTITY (1, 1) NOT NULL,
+    [AreaName]        VARCHAR (250)   NULL,
+    [CenterLat]       DECIMAL (13, 8) NULL,
+    [CenterLong]      DECIMAL (13, 8) NULL,
+    [CenterPoint]     AS              ([geography]::Point([CenterLat],[CenterLong],(4326))) PERSISTED,
+    [Radius]          INT             NULL,
+    [Area]            AS              ([geography]::Point([CenterLat],[CenterLong],(4326)).STBuffer([Radius])) PERSISTED,
+    [GeoLocBuffer]    AS              ([geography]::Point([CenterLat],[CenterLong],(4326)).STBuffer([Radius])) PERSISTED,
+    [Radius_3]        INT             NULL,
+    [Radius_5]        INT             NULL,
+    [Radius_10]       INT             NULL,
+    [Radius_15]       INT             NULL,
+    [GeoLocBuffer_3]  AS              ([geography]::Point([CenterLat],[CenterLong],(4326)).STBuffer([Radius_3])) PERSISTED,
+    [GeoLocBuffer_5]  AS              ([geography]::Point([CenterLat],[CenterLong],(4326)).STBuffer([Radius_5])) PERSISTED,
+    [GeoLocBuffer_10] AS              ([geography]::Point([CenterLat],[CenterLong],(4326)).STBuffer([Radius_10])) PERSISTED,
+    [GeoLocBuffer_15] AS              ([geography]::Point([CenterLat],[CenterLong],(4326)).STBuffer([Radius_15])) PERSISTED
 );
+
+
 
