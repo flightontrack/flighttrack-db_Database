2b88038b4c51809c2efe6d02139ec7bea9941d18
Tikhomirov <Tikhomirov@TIKHOMIR41360>
2017-05-08 10:13:53 -0400

    o

---
+++ flighttrack-db_Database/dbo/Tables/AircraftGroup_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AircraftGroup] (
+    [Id]      INT      IDENTITY (1, 1) NOT NULL,
+    [AcftID]  INT      NULL,
+    [GroupID] INT      NULL,
+    [Created] DATETIME DEFAULT (getdate()) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
