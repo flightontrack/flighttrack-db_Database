20fb2c79f76bef679c6b6a2241d0faeb34624cb2
Tikhomirov <Tikhomirov@TIKHOMIR41360>
2017-05-08 15:54:02 -0400

    o

--- flighttrack-db_Database/dbo/Stored Procedures/create_Pilot.sql
+++ flighttrack-db_Database/dbo/Stored Procedures/create_Pilot.sql
@@ -14,8 +14,8 @@ USING (SELECT PilotCode = ISNULL(@pilotcode,0),
 				SimNumber=ISNULL(@deviceid,0),
 				AnID=ISNULL(@androidid,0),
 				--UserId=@userid,
-				UserName=@username) AS s ON t.PilotCode=s.PilotCode AND t.SimNumber=s.SimNumber
-WHEN MATCHED THEN UPDATE SET PilotName=UserName
+				UserName=left(@username,3)+'-....'+right(@username,3)) AS s ON t.PilotCode=s.PilotCode AND t.SimNumber=s.SimNumber
+WHEN MATCHED THEN UPDATE SET PilotName=@username
 WHEN NOT MATCHED THEN INSERT (PilotCode,SimNumber,AnID,PilotName) VALUES (s.PilotCode,s.simNumber,s.AnID,s.UserName) 
 OUTPUT INSERTED.PilotID	AS PilotID;
 
